f595992d957509ca4e7c705e4ed23855e7935b0c
Prathamesh Mandiye <126768899+Prathamesh-Mandiye-0423@users.noreply.github.com>
2024-02-07 01:40:48 +0530

    Update and rename check1.cpp to cses_6.cpp

---
+++ cses-day-1/cses_6.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main(){
+    ll n,x;
+    cin>>n>>x;
+    vector<ll> p(n);
+    for(ll i=0;i<n;i++) cin>>p[i];
+    sort(p.begin(),p.end());
+    ll l=0;
+    ll r=n-1;
+    while(l<r){ //Two pointer 
+        if(p[l]+p[r]<=x){
+            n--;
+            l++;
+            r--;
+        }
+        else{
+            r--;
+        }
+    }
+    cout<<n<<'\n';
+
+}
