3d93ce221e6982c982f0085ffb4ac7baa196730c
Prathamesh Mandiye <126768899+Prathamesh-Mandiye-0423@users.noreply.github.com>
2024-02-10 00:47:28 +0530

    Update cses17.cpp

--- day 3/cses17.cpp
+++ day 3/cses17.cpp
@@ -1,1 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+#define ll long long
+#define ld long double
+
+
+int main(){
+    ll n;cin>>n;
+    vector<ll>v(n);
+    for(ll i=0;i<n;i++) cin>>v[i];
+    sort(v.begin(),v.end());
+    ll d=v[n/2];
+    ll ctr=0;
+    for(ll i=0;i<n;i++) ctr+=abs(v[i]-d);
+    cout<<ctr<<'\n';
+}
