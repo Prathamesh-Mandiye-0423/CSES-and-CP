24d7f3778c3d9aab998d0d31d4684d9991d5b647
Prathamesh Mandiye <126768899+Prathamesh-Mandiye-0423@users.noreply.github.com>
2024-02-09 23:23:48 +0530

    Add files via upload

---
+++ cses-day-2/cses16_2.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main(){
+  ll n;cin>>n;
+  vector<ll>x(n);
+  for(ll i=0;i<n;i++){
+    cin>>x[i];
+  }
+  ll max_val=INT_MIN;
+  ll curr=0;
+  for(ll i=0;i<n;i++){
+    if(curr+x[i]>=x[i]){ //if addition of curr value increases sum or decreases
+      curr+=x[i];
+    }
+    else{
+      curr=x[i];
+    }
+    max_val=max(max_val,curr);    
+  }
+  cout<<max_val<<'\n';
+}
