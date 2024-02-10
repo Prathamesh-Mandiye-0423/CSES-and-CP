085655b8b7088565a6d965b74ccbb8a9ce1f7a9b
Prathamesh Mandiye <126768899+Prathamesh-Mandiye-0423@users.noreply.github.com>
2024-02-11 00:18:37 +0530

    Create cses_25.cpp

---
+++ day-4/cses_25.cpp
@@ -0,0 +1,25 @@
+//Common divisor
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+const int N=1e7;
+int c[N+1];
+
+int main(){
+	int n;cin>>n;
+	for(int i=0;i<n;i++){
+		int x;cin>>x;
+		++c[x];
+	}
+	for(int i=N;i;--i){
+		int d=0;
+		for(int j=i;j<=N;j+=i){
+			d+=c[j];
+		}
+		if(d>1){
+			cout<<i;
+			break;
+		}
+	}
+}
